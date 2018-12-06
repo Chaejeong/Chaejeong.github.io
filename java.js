0e469d059d648e4e7926ab65e0196080bf8cb47d
Chaejeong <44868431+Chaejeong@users.noreply.github.com>
2018-12-06 21:39:06 +0900

    Update java.js

--- java.js
+++ java.js
@@ -3,7 +3,7 @@
         $.get("profile.json", function(data, status){
 
             $("#name").html("<b>이름: </b>" + data.name);
-            $("#home").html(""<b>사는 곳: </b>" + data.home);
+            $("#home").html("<b>사는 곳: </b>" + data.home);
             
         });
     });
