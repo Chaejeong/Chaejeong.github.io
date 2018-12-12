5ff3551aa5f0a33d32062449d00b6f515c9f2b86
Chaejeong <44868431+Chaejeong@users.noreply.github.com>
2018-12-12 21:32:05 +0900

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+    $("button").click(function(){
+        $.get("document.html", function(data, status){
+            alert("랜덤준비물은" + data + "입니다.");
+        });
+    });
+});
